21a695a47e665a68ff7a9c5238f0311bbd642feb
Robin Kalck <robin.kalck@le-campus-numerique.fr>
2022-05-02 15:58:58 +0200

    Etape 2 PHP Intro -- DISPLAY Header/Footer + Prix/Nom/Photo product

--- item.php
+++ item.php
@@ -1,2 +1,8 @@
 <?php
-$product_name = 
+include'header.php';
+$product_name = "6 oeufs";
+$product_price = "3,99€";
+echo "Nom de l'article: " . $product_name;
+echo "\nPrix de l'article: " . $product_price;
+echo '<img src="public\img\Oeufs.jpg" width="200"/>';
+include'footer.php';
