a86de75c67d329d63a7d2a28b5f4f102659a1228
Robin Kalck <robin.kalck@le-campus-numerique.fr>
2022-05-03 12:22:45 +0200

    Etape 4 -- simple-catalog UPDATE with loop

--- simple-catalog.php
+++ simple-catalog.php
@@ -5,5 +5,8 @@ $products = ["Oeuf",
     ];
 sort($products);
 var_dump($products);
+for ($i = 0; $i < count($products); $i++) {
+    echo "$products[$i]\n";
+    }
 echo $products[0],"/", $products[2];
 //echo $products[2];
