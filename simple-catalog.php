5b909f4d5bdf37faa6b0a4c95d1253d94f190268
Robin Kalck <robin.kalck@le-campus-numerique.fr>
2022-05-03 14:43:23 +0200

    Etape 4 -- Boucle

--- simple-catalog.php
+++ simple-catalog.php
@@ -4,9 +4,9 @@ $products = ["Oeuf",
     "Légumes",
     ];
 sort($products);
-var_dump($products);
+//var_dump($products);
 for ($i = 0; $i < count($products); $i++) {
     echo "$products[$i]\n";
     }
-echo $products[0],"/", $products[2];
+//echo $products[0],"/", $products[2];
 //echo $products[2];
