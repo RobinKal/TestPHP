f79958290d9b22d1e07bce05c6a68b493d985cb0
Robin Kalck <robin.kalck@le-campus-numerique.fr>
2022-05-16 14:16:09 +0200

    Revert "Etape 5 -- VAT Price + Excluded VAT Price + Discounted Price functions & display"
    This reverts commit 71cf40c3708668491dc6b2fedf70729c72a62a09.

--- multidimensional-catalog.php
+++ multidimensional-catalog.php
@@ -29,9 +29,7 @@ $products = [
 <?php foreach($products as $key) : ?>
     <div class="d-flex justify-content-center p-2">
          <h3><?php echo $key["name"]?></h3>
-    <p>Prix TTC : <?php echo formatPrice($key["price"])?></p>
-        <p>Prix HT : <?php echo priceExcludingVAT($key["price"])?></p>
-        <p>Discount : <?php echo $key["discount"]. "% / " . discountedPrice($key["price"],$key["discount"])?></p>
+    <p>Prix : <?php echo formatPrice($key["price"])?></p>
     <img src="<?php echo $key["picture_url"]?>" width="120" alt="Product picture">
 </div>
     <?php endforeach; ?>
--- my-functions.php
+++ my-functions.php
@@ -4,10 +4,5 @@ function formatPrice($price)
     return $price / 100 . "€";
 }
 function priceExcludingVAT($TTCPrice){
-    $HTPrice = (($TTCPrice*100)/(100+20)) / 100;
-    return number_format($HTPrice, 2) . "€";
-}
-function discountedPrice($TTCPrice, $discount){
-    $TTCPrice = ($TTCPrice - ($TTCPrice*$discount/100)) / 100;
-    return number_format($TTCPrice, 2) . "€";
+    return $TTCPrice*100/(100+20);
 }
